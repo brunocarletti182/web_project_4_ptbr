4bbbb914c663453471d11eefa8fefd813d8c41a2
Bruno Carletti <brunocarletti182@gmail.com>
2023-08-28 18:06:11 +0100

    v3

--- scripts/index.js
+++ scripts/index.js
@@ -20,12 +20,6 @@ popupProfileClose.addEventListener('click', function() {
   popupProfileContainer.classList.remove('popup__container_active');
 });
 
-likeIcons.forEach(likeIcon => {
-  likeIcon.addEventListener('click', function() {
-    likeIcon.classList.toggle('clicked');
-  });
-});
-
 popupProfileForm.addEventListener('submit', function(event) {
   event.preventDefault();
   overlay.classList.remove('popup__overlay_active');
@@ -34,6 +28,7 @@ popupProfileForm.addEventListener('submit', function(event) {
   let profInput = document.querySelector('.popup__profession').value;
   profileNameElement.textContent = nameInput;
   profileProfessionElement.textContent = profInput;
+
 });
 
 let popupImageOpen = document.querySelector('.profile__button');
@@ -50,85 +45,118 @@ popupImageClose.addEventListener('click', function() {
   popupImageContainer.classList.remove('popup__container_active');
 });
 
-
-
 const elementData = [
-  {
-    imageSrc: './assets/images/yosemite-valey.png',
-    legend: 'Vale de Yosemite'
-  },
-  {
-    imageSrc: './assets/images/louise-lake.png',
-    legend: 'Lago Louise'
-  },
-  {
-    imageSrc: './assets/images/montains.png',
-    legend: 'Montanhas Carelane'
-  },
-  {
-    imageSrc: './assets/images/vanoise.png',
-    legend: 'Vanoise'
-  },
-  {
-    imageSrc: './assets/images/latemar.png',
-    legend: 'Lago Louise'
-  },
-  {
-    imageSrc: './assets/images/di-braies-lake.png',
-    legend: 'Montanhas Carelane'
-  },
-  // Add more elements as needed
+  { imageSrc: './assets/images/yosemite-valey.png', legend: 'Vale de Yosemite' },
+  { imageSrc: './assets/images/louise-lake.png', legend: 'Lago Louise' },
+  { imageSrc: './assets/images/montains.png' , legend: 'Montanhas Carelane'},
+  { imageSrc: '../assets/images/latemar.png' , legend: 'Lago Latemar'},
+  { imageSrc: './assets/images/vanoise.png' , legend: 'Vanoise National'},
+  { imageSrc: './assets/images/di-braies-lake.png' , legend: 'Lago Di Braies'},
 ];
 
-function populateElementContainer() {
-  const elementContainer = document.querySelector('.element__container');
-  const elementTemplate = document.getElementById('elementTemplate');
+const elementContainer = document.querySelector('.element__container');
+const elementTemplate = document.querySelector('#elementTemplate');
 
-  // Loop through the element data and create the elements
-  for (const element of elementData) {
-    // Clone the template and populate it with data
-    const newElement = elementTemplate.content.cloneNode(true);
-    const imageElement = newElement.querySelector('.element__image');
-    const legendElement = newElement.querySelector('.element__legend-text');
+elementData.forEach((data, index) => {
+  const clonedTemplate = elementTemplate.content.cloneNode(true);
+  const elementItem = clonedTemplate.querySelector('.element__item');
+  const image = clonedTemplate.querySelector('.element__image');
+  const legendText = clonedTemplate.querySelector('.element__legend-text');
+  const likeIcon = clonedTemplate.querySelector('.element__like-icon');
+  const removeIcon = clonedTemplate.querySelector('.element__delete-icon'); // Get the remove icon
 
-    imageElement.src = element.imageSrc;
-    imageElement.alt += ` ${element.legend}`;
-    legendElement.textContent = element.legend;
+  image.src = data.imageSrc;
+  image.alt = data.legend;
+  legendText.textContent = data.legend;
 
-    elementContainer.appendChild(newElement);
-  }
-  function populateElementContainer() {
-    // ... (previous code for populating elements)
-
-    // Function to toggle the 'clicked' class on the like icon
-    function toggleLikeIcon(event) {
-      const likeIcon = event.target;
-      likeIcon.classList.toggle('clicked');
-    }
-
-    // Attach click event listeners to the like icons
-    const likeIcons = document.querySelectorAll('.element__like-icon');
-    likeIcons.forEach(likeIcon => {
-      likeIcon.addEventListener('click', toggleLikeIcon);
-    });
-
-     // Function to remove an element when the trash can icon is clicked
-  function removeElement(event) {
-    const elementItem = event.target.closest('.element__item');
-    if (elementItem) {
-      elementItem.remove();
-    }
-  }
-
-  // Attach click event listeners to the delete icons
-  const deleteIcons = document.querySelectorAll('.element__delete-icon');
-  deleteIcons.forEach(deleteIcon => {
-    deleteIcon.addEventListener('click', removeElement);
+  likeIcon.addEventListener('click', function() {
+    likeIcon.classList.toggle('clicked');
   });
-  }
 
-  populateElementContainer();
+  removeIcon.addEventListener('click', function() { // Add event listener to the remove icon
+    elementContainer.removeChild(elementItem);
+    // You might also want to update your elementData array to reflect the removal
+  });
 
+  elementContainer.appendChild(clonedTemplate);
+});
+
+let popupImageSave = document.querySelector('.popup-image__save'); // Get the "Adicionar" button
+
+function setupDeleteIcon(deleteIcon, elementItem, imageSrc, legend) {
+  deleteIcon.addEventListener('click', function() {
+    elementContainer.removeChild(elementItem);
+    // You might also want to update your elementData array to reflect the removal
+  });
+
+  // Add a click event listener to the image
+  imageSrc.addEventListener('click', function() {
+    imageViewerImage.src = imageSrc.src;
+    imageViewerImage.alt = legend;
+
+    const legendContainer = document.querySelector('.popup-image-viewer__legend');
+    legendContainer.textContent = legend;
+
+    imageViewerContainer.classList.add('popup__container_active');
+    overlay.classList.add('popup__overlay_active');
+  });
 }
 
-populateElementContainer();
+popupImageSave.addEventListener('click', function() {
+  let titleInput = document.querySelector('.popup-image__local').value;
+  let imageUrlInput = document.querySelector('.popup-image__url').value;
+
+  // Create a new element using the template
+  const clonedTemplate = elementTemplate.content.cloneNode(true);
+  const elementItem = clonedTemplate.querySelector('.element__item');
+  const image = clonedTemplate.querySelector('.element__image');
+  const legendText = clonedTemplate.querySelector('.element__legend-text');
+  const likeIcon = clonedTemplate.querySelector('.element__like-icon');
+  const removeIcon = clonedTemplate.querySelector('.element__delete-icon'); // Get the new remove icon
+
+  image.src = imageUrlInput;
+  image.alt = titleInput;
+  legendText.textContent = titleInput;
+
+  likeIcon.addEventListener('click', function() {
+    likeIcon.classList.toggle('clicked');
+  });
+
+  setupDeleteIcon(removeIcon, elementItem, image, titleInput); // Call the setupDeleteIcon function
+
+  // Insert the newly created element at the beginning of elementContainer
+  elementContainer.insertBefore(clonedTemplate, elementContainer.firstChild);
+
+  // Close the popup
+  overlay.classList.remove('popup__overlay_active');
+  popupImageContainer.classList.remove('popup__container_active');
+
+  // Clear input fields
+  document.querySelector('.popup-image__local').value = '';
+  document.querySelector('.popup-image__url').value = '';
+});
+
+
+const imageViewerContainer = document.querySelector('.popup-image-viewer__container');
+const imageViewerImage = document.querySelector('.popup-image-viewer__image');
+const imageViewerClose = document.querySelector('.popup-image-viewer__close');
+
+document.querySelectorAll('.element__image').forEach((image, index) => {
+  image.addEventListener('click', function() {
+    const imageData = elementData[index]; // Get the data for the clicked image
+    imageViewerImage.src = imageData.imageSrc;
+    imageViewerImage.alt = imageData.legend;
+
+    const legendContainer = document.querySelector('.popup-image-viewer__legend');
+    legendContainer.textContent = imageData.legend;
+
+    imageViewerContainer.classList.add('popup__container_active');
+    overlay.classList.add('popup__overlay_active');
+  });
+});
+
+// Add event listener to close the image viewer popup
+imageViewerClose.addEventListener('click', function() {
+  imageViewerContainer.classList.remove('popup__container_active');
+  overlay.classList.remove('popup__overlay_active');
+});
