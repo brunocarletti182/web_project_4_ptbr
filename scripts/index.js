6c85bbba1724b6998fc71944b91d9460b534b733
Bruno Carletti <brunocarletti182@gmail.com>
2023-08-29 21:33:42 +0100

    v3

--- scripts/index.js
+++ scripts/index.js
@@ -32,7 +32,7 @@ popupProfileForm.addEventListener('submit', function(event) {
 });
 
 let popupImageOpen = document.querySelector('.profile__button');
-let popupImageClose = document.querySelector('.popup-image__close');
+let popupImageClose = document.querySelector('.popup__close_image');
 let popupImageContainer = document.querySelector('.popup__container_image');
 
 popupImageOpen.addEventListener('click', function() {
@@ -81,7 +81,7 @@ elementData.forEach((data, index) => {
   elementContainer.appendChild(clonedTemplate);
 });
 
-let popupImageSave = document.querySelector('.popup-image__save');
+let popupImageSave = document.querySelector('.popup__save_image');
 
 function setupDeleteIcon(deleteIcon, elementItem, imageSrc, legend) {
   deleteIcon.addEventListener('click', function() {
@@ -94,7 +94,7 @@ function setupDeleteIcon(deleteIcon, elementItem, imageSrc, legend) {
     imageViewerImage.src = imageSrc.src;
     imageViewerImage.alt = legend;
 
-    const legendContainer = document.querySelector('.popup-image-viewer__legend');
+    const legendContainer = document.querySelector('.popup__viewer_legend');
     legendContainer.textContent = legend;
 
     imageViewerContainer.classList.add('popup__container_active');
@@ -137,9 +137,9 @@ popupImageSave.addEventListener('click', function() {
 });
 
 
-const imageViewerContainer = document.querySelector('.popup-image-viewer__container');
-const imageViewerImage = document.querySelector('.popup-image-viewer__image');
-const imageViewerClose = document.querySelector('.popup-image-viewer__close');
+const imageViewerContainer = document.querySelector('.popup__viewer');
+const imageViewerImage = document.querySelector('.popup__viewer_image');
+const imageViewerClose = document.querySelector('.popup__viewer_close');
 
 document.querySelectorAll('.element__image').forEach((image, index) => {
   image.addEventListener('click', function() {
@@ -147,7 +147,7 @@ document.querySelectorAll('.element__image').forEach((image, index) => {
     imageViewerImage.src = imageData.imageSrc;
     imageViewerImage.alt = imageData.legend;
 
-    const legendContainer = document.querySelector('.popup-image-viewer__legend');
+    const legendContainer = document.querySelector('.popup__viewer_legend');
     legendContainer.textContent = imageData.legend;
 
     imageViewerContainer.classList.add('popup__container_active');
