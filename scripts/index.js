4e014121b48b0351c8449ce10d9a13ebcee0eb3a
Bruno Carletti <brunocarletti182@gmail.com>
2023-09-22 21:35:30 +0100

    v7

--- scripts/index.js
+++ scripts/index.js
@@ -9,6 +9,17 @@ const profileProfessionElement = document.querySelector('.profile__profession');
 const likeIcons = document.querySelectorAll('.element__like-icon');
 const overlay = document.querySelector('.popup__overlay');
 
+// Open and close image popup
+const popupImageOpen = document.querySelector('.profile__button');
+const popupImageClose = document.querySelector('.popup__close_image');
+const popupImageContainer = document.querySelector('.popup__container_image');
+
+// Image Viewer
+const popupImageSave = document.querySelector('.popup__save_image');
+const imageViewerContainer = document.querySelector('.popup-viewer');
+const imageViewerImage = document.querySelector('.popup-viewer__image');
+const imageViewerClose = document.querySelector('.popup-viewer__close');
+
 // Event listeners for opening and closing profile popup
 popupProfileOpen.addEventListener('click', function() {
   overlay.classList.add('popup__overlay_active');
@@ -18,6 +29,16 @@ popupProfileOpen.addEventListener('click', function() {
 popupProfileClose.addEventListener('click', function() {
   overlay.classList.remove('popup__overlay_active');
   popupProfileContainer.classList.remove('popup__container_active');
+});
+
+popupImageOpen.addEventListener('click', function() {
+  overlay.classList.add('popup__overlay_active');
+  popupImageContainer.classList.add('popup__container_active');
+});
+
+popupImageClose.addEventListener('click', function() {
+  overlay.classList.remove('popup__overlay_active');
+  popupImageContainer.classList.remove('popup__container_active');
 });
 
 // Close all popups
@@ -65,19 +86,10 @@ document.addEventListener('keydown', function(event) {
   }
 });
 
-// Open and close image popup
-const popupImageOpen = document.querySelector('.profile__button');
-const popupImageClose = document.querySelector('.popup__close_image');
-const popupImageContainer = document.querySelector('.popup__container_image');
-
-popupImageOpen.addEventListener('click', function() {
-  overlay.classList.add('popup__overlay_active');
-  popupImageContainer.classList.add('popup__container_active');
-});
-
-popupImageClose.addEventListener('click', function() {
+// Close image viewer
+imageViewerClose.addEventListener('click', function() {
+  imageViewerContainer.classList.remove('popup__container_active');
   overlay.classList.remove('popup__overlay_active');
-  popupImageContainer.classList.remove('popup__container_active');
 });
 
 // data for elements images
@@ -125,12 +137,6 @@ elementData.forEach((data, index) => {
   // Append element to container
   elementContainer.appendChild(clonedTemplate);
 });
-
-// Image Viewer
-const popupImageSave = document.querySelector('.popup__save_image');
-const imageViewerContainer = document.querySelector('.popup-viewer');
-const imageViewerImage = document.querySelector('.popup-viewer__image');
-const imageViewerClose = document.querySelector('.popup-viewer__close');
 
 // Function to set up delete icon and open image viewer
 function setupDeleteIcon(deleteIcon, elementItem, imageSrc, legend) {
@@ -203,8 +209,4 @@ document.querySelectorAll('.element__image').forEach((image, index) => {
   });
 });
 
-// Close image viewer
-imageViewerClose.addEventListener('click', function() {
-  imageViewerContainer.classList.remove('popup__container_active');
-  overlay.classList.remove('popup__overlay_active');
-});
+
